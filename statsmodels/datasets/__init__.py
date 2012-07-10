6fe8ba56b9628ac557ae3be84539416d143711cc
Alexandre Crayssac <alexandre.crayssac@gmail.com>
2012-07-10 16:55:11 +0200

    Added Kmenta dataset

--- statsmodels/datasets/__init__.py
+++ statsmodels/datasets/__init__.py
@@ -5,6 +5,5 @@ Datasets module
 from . import (anes96, cancer, committee, ccard, copper, cpunish, elnino,
                engel, grunfeld, longley, macrodata, modechoice, nile, randhie,
                scotland, spector, stackloss, star98, strikes, sunspots, fair,
-               heart, statecrime, co2, fertility, munnell)
+               heart, statecrime, co2, fertility, munnell, kmenta)
 from .utils import get_rdataset, get_data_home, clear_data_home, webuse, check_internet
-
